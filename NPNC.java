abe07feb4b53bf04bc5f07671f2146819fa75134
Vladimir Ascencio <vladimirramos405@gmail.com>
2024-05-24 14:57:56 -0600

    Numero_Positivo_Negativo_Cero

---
+++ NPNC.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NPNC {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        double num;
+
+        System.out.print("\nDigite el número: ");
+        num = entrada.nextDouble();
+
+        if (num > 0) {
+            System.out.println("\nEl número es positivo.");
+            System.out.println("Fin del programa.\n");
+        } else if (num < 0) {
+            System.out.println("\nEl número es negativo.");
+            System.out.println("Fin del programa.\n");
+        } else {
+            System.out.println("\nEl número es 0.");
+            System.out.println("Fin del programa.\n");
+        }
+
+        entrada.close();
+    }
+}
