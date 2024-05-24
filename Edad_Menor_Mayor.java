7f14eae60b6efcdf0033841244d1cbd4f6ce223c
Vladimir Ascencio <vladimirramos405@gmail.com>
2024-05-24 15:01:43 -0600

    Mayor o Menor de Edad

---
+++ Edad_Menor_Mayor.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Edad_Menor_Mayor {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int edad;
+
+        System.out.print("\n\nDigite su edad: ");
+        edad = entrada.nextInt();
+
+        if (edad > 18) {
+            System.out.println("\nEs mayor de edad, ya puede tomar.");
+            System.out.println("Fin del programa.\n");
+        } else {
+            System.out.println("\nEs menor de edad, no puede tomar alcohol.");
+            System.out.println("Fin del programa.\n");
+        }
+
+        entrada.close();
+    }
+}
