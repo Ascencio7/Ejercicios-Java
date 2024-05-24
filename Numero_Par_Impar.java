862194377f3445de95d07a952755904480e570bc
Vladimir Ascencio <vladimirramos405@gmail.com>
2024-05-24 14:39:49 -0600

    Numero Par o Impar

---
+++ Numero_Par_Impar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Numero_Par_Impar {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        double num;
+
+        System.out.print("\nDigite el número a verificar si es par o impar: ");
+        num = entrada.nextDouble();
+
+        if (num % 2 == 0) {
+            System.out.println("\nEl número es par.\n");
+        } else {
+            System.out.println("\nEl número es impar.\n");
+        }
+
+        entrada.close();
+    }
+}
