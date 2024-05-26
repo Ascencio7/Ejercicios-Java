0ff9833ce2af07a035632865f86f4241ed132cd3
Vladimir Ascencio <vladimirramos405@gmail.com>
2024-05-25 20:13:29 -0600

    Tablas de multiplicar con While

---
+++ Tabla_Multiplicar.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Tabla_Multiplicar {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int num;
+        int i = 1;
+
+        System.out.print("\nDigite el número de la tabla: ");
+        num = entrada.nextInt();
+
+        System.out.println("\nTabla de multiplicar del " + num + ":\n");
+
+        while (i <= 10) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+            i++;
+        }
+
+        System.out.println("\n");
+
+        entrada.close();
+    }
+}
