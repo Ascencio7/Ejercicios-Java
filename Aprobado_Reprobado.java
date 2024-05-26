330555e8139f9ee3ccae09bb2607fa74be6bc77b
Vladimir Ascencio <vladimirramos405@gmail.com>
2024-05-25 19:30:21 -0600

    Aprobado o Reprobado

--- Aprobado_Reprobado.java
+++ Aprobado_Reprobado.java
@@ -1,3 +1,23 @@
-public class Aprobado_Reprobado {
+import java.util.Scanner;
 
+public class Aprobado_Reprobado {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        double nota;
+
+        System.out.print("\nDigite la nota del estudiante: ");
+        nota = entrada.nextDouble();
+
+        if (nota > 7 && nota < 10) {
+            System.out.println("\nEl estudiante ha aprobado la materia.");
+            System.out.println("Fin del programa.\n");
+        } else if (nota < 7) {
+            System.out.println("\nEl estudiante ha reprobado la materia.");
+            System.out.println("Fin del programa.\n");
+        } else {
+            System.out.println("\nError");
+        }
+        entrada.close();
+    }
 }
